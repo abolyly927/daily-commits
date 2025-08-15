28de54962dea3ebf6a225088db61f5f0f881563f
aamt39239 <aamt39239@gmail.com>
2025-08-15 13:45:48 +0200

    Daily commit for Fri 08/15/2025 at 13:45:48.46

--- daily-commit.bat
+++ daily-commit.bat
@@ -1,6 +1,7 @@
-cd /d "C:\Users\anas\Desktop\daily-commits-github"
-echo Daily commit for %date% at %time% >> log.txt
+cd /d "C:\Users\anas\Desktop\daily-commits"
+echo Commit at %date% %time% >> log.txt
+type nul > temp.txt
 git add .
-git commit -m "Daily commit for %date%"
+git commit -m "Daily commit for %date% at %time%"
 git push origin main
 pause
