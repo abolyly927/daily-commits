491adffb642125900f75c302e64c639fdf161ea2
aamt39239 <aamt39239@gmail.com>
2025-08-16 11:57:01 +0200

    Daily commit for Sat 08/16/2025 at 11:57:01.09

--- commit_aamt39239.bat
+++ commit_aamt39239.bat
@@ -1,7 +1,8 @@
 cd /d "C:\Users\anas\Desktop\daily-commits"
+git config user.name "aamt39239"
+git config user.email "aamt39239@gmail.com"
 echo Commit at %date% %time% >> log.txt
-type nul > temp.txt
 git add .
-git commit -m "Daily commit for %date% at %time%"
+git commit --allow-empty -m "Daily commit for %date% at %time%"
 git push origin main
 pause
