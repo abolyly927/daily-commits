5b14b89277d552a2f20158d785f69a441842066e
aamt39239 <aamt39239@gmail.com>
2025-08-17 11:51:27 +0200

    Commit made automatically at Sun 08/17/2025 11:51:27.27

--- commit_aamt39239.bat
+++
@@ -1,8 +0,0 @@
-cd /d "C:\Users\anas\Desktop\daily-commits"
-git config user.name "aamt39239"
-git config user.email "aamt39239@gmail.com"
-echo Commit at %date% %time% >> log.txt
-git add .
-git commit --allow-empty -m "Daily commit for %date% at %time%"
-git push origin main
-pause
--- daily-commit.bat
+++
@@ -1,7 +0,0 @@
-cd /d "C:\Users\anas\Desktop\daily-commits"
-echo Commit at %date% %time% >> log.txt
-type nul > temp.txt
-git add .
-git commit -m "Daily commit for %date% at %time%"
-git push origin main
-pause
