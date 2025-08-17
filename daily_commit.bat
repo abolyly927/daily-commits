64e4620b589ec4f7cbdd066929fa61d63a2d2a52
aamt39239 <aamt39239@gmail.com>
2025-08-17 11:53:45 +0200

    Commit made automatically at Sun 08/17/2025 11:53:45.84

--- daily_commit.bat
+++ daily_commit.bat
@@ -18,3 +18,23 @@ REM رفع التغييرات
 git push origin main
 
 pause
+@echo off
+REM تحديد مسار المستودع
+cd /d "%~dp0"
+
+REM تحديث المستودع
+git pull origin main
+
+REM إضافة ملف أو تعديل ملف بتاريخ اليوم (مثال: daily.txt)
+echo Commit at %date% %time% >> daily.txt
+
+REM إضافة التغييرات
+git add .
+
+REM عمل commit مع رسالة بتاريخ ووقت
+git commit -m "Commit made automatically at %date% %time%"
+
+REM رفع التغييرات
+git push origin main
+
+pause
